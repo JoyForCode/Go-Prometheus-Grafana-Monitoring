828e3a773c4ca45ffd3a67f3d0bce96c7ff6883f
Jignesh Atul Paratane <jigneshparatane@gmail.com>
2026-02-12 21:33:35 +0530

    Added the module for Server PSU monitoring metrics, added psu consumed watts and psu capacity watts as a Gauge metric to reflect dynamic changes in the readings of the metrics.

--- metrics.go
+++ metrics.go
@@ -1,7 +1,31 @@
 package main
 
-type Power struct {
-	PSU []struct {
-		PowerConsumedWatts float64 `json:"PowerConsumedWatts"`
-	} `json:"PSU"`
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	psuWatts = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "power_consumed_watts",
+			Help: "Current PSU power consumption in watts",
+		},
+	)
+
+	psuCapacityWatts = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name:"power_capacity_watts",
+			Help:"Maximum PSU power capacity in watts",
+		},
+	)
+)
+
+func InitMetrics() {
+	prometheus.MustRegister(psuWatts)
+	prometheus.MustRegister()
+}
+
+func UpdateMetrics(power *PowerControl) {
+	psuWatts.Set(power.PowerConsumedWatts)
+	psuCapacityWatts.Set(power.PowerCapacityWatts)
 }
