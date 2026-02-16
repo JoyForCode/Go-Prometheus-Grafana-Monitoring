d5d5b0e3abb55bf18a28db974fe6eba76a793698
Jignesh Atul Paratane <jigneshparatane@gmail.com>
2026-02-16 15:46:15 +0530

    Added several other metrics to be monitored, and updated the Name and Help to get appropriate meanings for the metrics. #2

--- internal/metrics/temperature_metrics.go
+++ internal/metrics/temperature_metrics.go
@@ -9,58 +9,84 @@ import (
 )
 
 var (
-	ReadingCelsius = prometheus.NewGaugeVec(
+	temperatureCelsius = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Name: "current_temperature_reading_celsius",
-			Help: "Current temperature reading in Celsius",
-		}, []string{"name", "physical_context", "sensor_number"},
+			Name: "redfish_temperature_celsius",
+			Help: "Temperature sensor reading in Celsius.",
+		},
+		[]string{"sensor", "physical_context", "sensor_number"},
+	)
+
+	temperatureLowerCritical = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "redfish_temperature_lower_threshold_critical_celsius",
+			Help: "Lower critical temperature threshold in Celsius.",
+		},
+		[]string{"sensor", "physical_context", "sensor_number"},
+	)
+
+	temperatureLowerFatal = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "redfish_temperature_lower_threshold_fatal_celsius",
+			Help: "Lower fatal temperature threshold in Celsius.",
+		},
+		[]string{"sensor", "physical_context", "sensor_number"},
+	)
+
+	temperatureUpperCritical = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "redfish_temperature_upper_threshold_critical_celsius",
+			Help: "Upper critical temperature threshold in Celsius.",
+		},
+		[]string{"sensor", "physical_context", "sensor_number"},
+	)
+
+	temperatureUpperFatal = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "redfish_temperature_upper_threshold_fatal_celsius",
+			Help: "Upper fatal temperature threshold in Celsius.",
+		},
+		[]string{"sensor", "physical_context", "sensor_number"},
 	)
 )
 
 func InitTemperatureMetrics() {
-	prometheus.MustRegister(ReadingCelsius)
+	prometheus.MustRegister(
+		temperatureCelsius,
+		temperatureLowerCritical,
+		temperatureLowerFatal,
+		temperatureUpperCritical,
+		temperatureUpperFatal,
+	)
 }
 
-func UpdateTemperatureMetrics(temperature *models.Temperature) {
-	if temperature.ReadingCelsius == nil {
+func UpdateTemperatureMetrics(temp *models.Temperature) {
+
+	if temp.ReadingCelsius == nil {
 		return
 	}
 
-	ReadingCelsius.With(prometheus.Labels{
-		"name":             temperature.Name,
-		"physical_context": temperature.PhysicalContext,
-		"sensor_number":    strconv.Itoa(temperature.SensorNumber),
-	}).Set(*temperature.ReadingCelsius)
-}
-
-func mapHeathToFloat(health *string) float64 {
-	if health == nil {
-		return -1
+	labels := prometheus.Labels{
+		"sensor":           temp.Name,
+		"physical_context": temp.PhysicalContext,
+		"sensor_number":    strconv.Itoa(temp.SensorNumber),
 	}
 
-	switch *health {
-	case "OK":
-		return 1
-	case "Warning":
-		return 0.5
-	case "Critical":
-		return 0
-	default:
-		return -1
-	}
-}
+	temperatureCelsius.With(labels).Set(*temp.ReadingCelsius)
 
-func mapStateToFloat(s *string) float64 {
-	if s == nil {
-		return -1
+	if temp.LowerThresholdCritical != nil {
+		temperatureLowerCritical.With(labels).Set(*temp.LowerThresholdCritical)
 	}
 
-	switch *s {
-	case "Enabled":
-		return 1
-	case "Disabled":
-		return 0
-	default:
-		return -1
+	if temp.LowerThresholdFatal != nil {
+		temperatureLowerFatal.With(labels).Set(*temp.LowerThresholdFatal)
+	}
+
+	if temp.UpperThresholdCritical != nil {
+		temperatureUpperCritical.With(labels).Set(*temp.UpperThresholdCritical)
+	}
+
+	if temp.UpperThresholdFatal != nil {
+		temperatureUpperFatal.With(labels).Set(*temp.UpperThresholdFatal)
 	}
 }
