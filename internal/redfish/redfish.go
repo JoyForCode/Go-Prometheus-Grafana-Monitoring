c647688e799b7ca3857088b04294952db722b5ed
Jignesh Atul Paratane <jigneshparatane@gmail.com>
2026-02-16 14:45:27 +0530

    Added a RetrieveTempertureValues() test method to retrieve the temperature metrics at a specific endpoint. #2

--- internal/redfish/redfish.go
+++ internal/redfish/redfish.go
@@ -59,3 +59,48 @@ func RetrievePowerValues() (*models.PowerControl, error) {
 
 	return &power, nil
 }
+
+func RetrieveTemperatureValues() (*models.Temperature, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   30 * time.Second,
+	}
+
+	req, err := http.NewRequest(
+		"GET",
+		"https://192.168.1.101/redfish/v1/Chassis/System.Embedded.1/Sensors/Temperatures/iDRAC.Embedded.1%23SystemBoardExhaustTemp",
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetBasicAuth("root", "calvin")
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	var temperature models.Temperature
+	err = json.NewDecoder(resp.Body).Decode(&temperature)
+	if err != nil {
+		return nil, err
+	}
+
+	return &temperature, nil
+}
