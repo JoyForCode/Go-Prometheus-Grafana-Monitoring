706ab5943bec7e21d91cf8c699cbba1baf52e688
Jignesh Atul Paratane <jigneshparatane@gmail.com>
2026-02-17 12:07:37 +0530

    Defined the Client struct in client.go module, this struct is universal for all the client creations. #3

--- internal/redfish/client.go
+++ internal/redfish/client.go
@@ -1,6 +1,14 @@
 package redfish
 
+import (
+	"net/http"
+)
+
 type Client struct {
+	BaseURL    string
+	Username   string
+	Password   string
+	HTTPClient *http.Client
 }
 
 /*The client.go file serves as a centrailzed client creation for all HTTP requests, and allows using same client for different endpoint types.*/
