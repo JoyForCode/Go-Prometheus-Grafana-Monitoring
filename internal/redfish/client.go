a196d5b7f3b17087b6dd0f735f970c3560994bf1
Jignesh Atul Paratane <jigneshparatane@gmail.com>
2026-02-17 11:55:58 +0530

    Added a empty client.go file with a empty defined struct, which will later be written upon structure clarfiication of client calling stages and HTTP requests processes. #3

---
+++ internal/redfish/client.go
@@ -0,0 +1,6 @@
+package redfish
+
+type Client struct {
+}
+
+/*The client.go file serves as a centrailzed client creation for all HTTP requests, and allows using same client for different endpoint types.*/
