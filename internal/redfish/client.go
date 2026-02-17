dc00678ed8cdf8243e72001460b049b39e274197
Jignesh Atul Paratane <jigneshparatane@gmail.com>
2026-02-17 17:28:39 +0530

    Added the Constructor for calling the Client struct and initialized the http request logic with TLS verification to off. #3

--- internal/redfish/client.go
+++ internal/redfish/client.go
@@ -1,7 +1,9 @@
 package redfish
 
 import (
+	"time"
 	"net/http"
+	"crypto/tls"
 )
 
 type Client struct {
@@ -9,6 +11,24 @@ type Client struct {
 	Username   string
 	Password   string
 	HTTPClient *http.Client
+}
+
+func NewClient(baseURL, username, password string) *Client {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	return &Client{
+		BaseURL: baseURL,
+		Username: username,
+		Password: password,
+		HTTPClient: &http.Client{
+			Transport: tr,
+			Timeout: 15 * time.Second,
+		},
+	}
 }
 
 /*The client.go file serves as a centrailzed client creation for all HTTP requests, and allows using same client for different endpoint types.*/
