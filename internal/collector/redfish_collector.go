9c4a2c2a3044595ee4019972970770be7e325145
Jignesh Atul Paratane <jigneshparatane@gmail.com>
2026-02-18 22:51:16 +0530

    Added a Describe() method standard to Collector interface, and created a new constructor to initialize metrics. #5

--- internal/collector/redfish_collector.go
+++ internal/collector/redfish_collector.go
@@ -13,3 +13,36 @@ type RedfishCollector struct {
 	powerCapacityDesc *prometheus.Desc
 	redfishUpDesc     *prometheus.Desc
 }
+
+func NewRedfishCollector(client RedfishAPI) *RedfishCollector {
+	return &RedfishCollector{
+		client: client,
+		powerConsumedDesc: prometheus.NewDesc(
+			"power_consumed_watts",
+			"Current PSU power consumption in watts",
+			nil,
+			nil,
+		),
+
+		powerCapacityDesc: prometheus.NewDesc(
+			"power_capacity_watts",
+			"PSU power capacity in watts",
+			nil,
+			nil,
+		),
+
+		redfishUpDesc: prometheus.NewDesc(
+			"redfish_up",
+			"Redfish API Response Status (1 = up, 0 = down)",
+			nil,
+			nil,
+		),
+	}
+}
+
+func (c *RedfishCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.powerConsumedDesc
+	ch <- c.powerCapacityDesc
+	ch <- c.redfishUpDesc
+}
+
